f3080914a19b7ccc3f805228a47cafed97e8d165
Austin Holmes <austin.holmes4@gmail.com>
2023-03-08 17:36:47 -0700

    imported requests and set up code for scraping url

--- scraper.py
+++ scraper.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
+import requests
+
+    # url of the website you want to scrape
+url = ""
+    # uses requests library to perform a get request
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+    # finding prices of a certain commodity on a website
+prices = doc.find_all(text="$")
+prices[0].parent
+
 
 with open("index.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
 
-tag = doc.title
-print(tag)
+tags = doc.find_all("p")
+print(tags)
